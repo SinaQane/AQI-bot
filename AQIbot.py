3158c18b0932ec80cdb3746568b61c83a97fa647
Sina Ghaseminejad <sina.qane@yahoo.com>
2019-12-17 16:06:19 +0330

    Update AQIbot.py

--- AQIbot.py
+++ AQIbot.py
@@ -1,5 +1,7 @@
 import telegram
+import requests
 import logging
+import json
 
 botToken = "TOKEN"
 
@@ -22,10 +24,45 @@ def help(update, context):
 
 def aqiranges(update, context):
 	update.message.reply_text('Every category corresponds to a different level of health concern. The six levels of health concern and what they mean are: \n' + 'Good: AQI is 0 to 50. Air quality is considered satisfactory, and air pollution poses little or no risk. \n' + 'Moderate: AQI is 51 to 100. Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people. For example, people who are unusually sensitive to ozone may experience respiratory symptoms. \n' + 'Unhealthy for Sensitive Groups: AQI is 101 to 150. Although general public is not likely to be affected at this AQI range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas persons with heart and lung disease, older adults and children are at greater risk from the presence of particles in the air. \n' + 'Unhealthy: AQI is 151 to 200. Everyone may begin to experience some adverse health effects, and members of the sensitive groups may experience more serious effects. \n' + 'Very Unhealthy: AQI is 201 to 300. This would trigger a health alert signifying that everyone may experience more serious health effects. \n' + 'Hazardous: AQI greater than 300. This would trigger a health warnings of emergency conditions. The entire population is more likely to be affected.')
+
+def aqilevelcheck (aqinumber):
+	if aqinumber in range (0,51):
+		return ("Good")
+	elif aqinumber in range (51,101):
+		return ("Moderate")
+	elif aqinumber in range (101,151):
+		return ("Unhealthy for sensitive groups")
+	elif aqinumber in range (151,201):
+		return ("Unhealthy")
+	elif aqinumber in range (201,301):
+		return ("Very Unhealthy")
+	elif aqinumber in range (301,501):
+		return ("Hazardous")
+	else:
+		return ("There seems to be an error or Air Quality Index is out of range.")
+
+def airnowregions (city):
+	request = requests.get('https://api.waqi.info/search/?token='+token=botToken+'&keyword='+city)
+	res = json.loads(request.text)
+	resdic = res.get('data')
+	stations =[]
+	for i in range (0,len(resdic)):
+    		stations.append (resdic[i].get('station'))
+    		if resdic[i].get('aqi') == '-':
+        		stations.append ("No Data Available")
+    		else:
+       			stations.append (resdic[i].get('aqi'))
+
+	stations2=[]
+	for i in range (0 , len(stations),2):
+    		stations2.append (stations[i].get('name'))
+    		stations2.append (stations[i+1])
+
+	finalstr = ""
+	for i in range (0 , len(stations),2):
+    		finalstr = finalstr + stations2[i] +" :" + stations2[i+1] +"\n"
+	return("For" + city + "regions, we have results below: \n" + finalstr)
 	
-def airnow (city):
-	
-				
 def error(update, context):
 	logger.warning('Update "%s" caused error "%s"', update, context.error)
 
